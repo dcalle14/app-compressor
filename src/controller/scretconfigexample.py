586fece093bc7daa5e30180a727025769d78ad8c
dcalle14 <140743146+dcalle14@users.noreply.github.com>
2024-11-07 17:36:24 -0500

    Create scretconfigexample.py
    configuracion de la conexion a la base de datos

---
+++ src/controller/scretconfigexample.py
@@ -0,0 +1,7 @@
+# scretconfigexample.py
+
+PGDATABASE = "nombre_de_tu_base_de_datos"
+PGUSER = "tu_usuario"
+PGPASSWORD = "tu_contraseña"
+PGHOST = "localhost"  # O la dirección IP de tu servidor de base de datos
+PGPORT = "5432"       # Puerto de PostgreSQL, 5432 es el puerto por defecto
